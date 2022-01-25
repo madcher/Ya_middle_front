638c177d3b3fe9ea054d9b333ac6623625c90053
Maxim <madcher@yandex.ru>
2022-01-25 23:01:08 +0300

    Create fetch.js

---
+++ practic/fetch.js
@@ -0,0 +1,76 @@
+const METHODS = {
+    GET:'GET',
+    POST:'POST',
+    PUT:'PUT',
+    PATCH:'PATCH',
+    DELETE:'DELETE',
+};
+
+/**
+	* Функцию реализовывать здесь необязательно, но может помочь не плодить логику у GET-метода
+	* На входе: объект. Пример: {a: 1, b: 2, c: {d: 123}, k: [1, 2, 3]}
+	* На выходе: строка. Пример: ?a=1&b=2&c=[object Object]&k=1,2,3
+*/
+
+
+function queryStringify(data) {
+  // Можно делать трансформацию GET-параметров в отдельной функции
+  if (typeof data !== 'object') {
+    return '';
+  }
+  let result = '?';
+  
+  Object.keys(data).forEach(key => {
+    result += `${key}=${data[key]}&`;
+  });
+  return result.slice(0, -1); ;
+}
+
+class HTTPTransport {
+		get = (url, options = {}) => {
+				 
+				return this.request(url, {...options, method: METHODS.GET}, options.timeout);
+		};
+
+		// PUT, POST, DELETE
+        
+        put = (url, options = {}) => {
+			return this.request(url, {...options, method: METHODS.PUT}, options.timeout);
+		};
+        post = (url, options = {}) => {
+			return this.request(url, {...options, method: METHODS.POST}, options.timeout);
+		};
+        delete = (url, options = {}) => {
+			return this.request(url, {...options, method: METHODS.DELETE}, options.timeout);
+		};
+		// options:
+		// headers — obj
+		// data — obj
+		request = (url, options, timeout = 5000) => {
+			const {method, data} = options;
+
+            return new Promise((resolve, reject) => {
+              const xhr = new XMLHttpRequest();
+              
+              url = method === METHODS.GET ? url + queryStringify(data) : url;
+              setTimeout(() => {
+                reject('timeout');
+              }, timeout);
+              xhr.open(method, url);
+
+              xhr.onload = function() {
+                resolve(xhr);
+              };
+              
+              xhr.onabort = reject;
+              xhr.onerror = reject;
+              xhr.ontimeout = reject;
+
+              if (method === METHODS.GET || !data) {
+                xhr.send();
+              } else {
+                xhr.send(data);
+              }
+            });	
+		};
+}
